439fcc8b429cddd3d23e0b86eed87b02a33680f6
João Henrik <jh.signori@gmail.com>
2022-06-18 11:42:27 -0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,26 @@ import './App.css'
 
 export function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <header>
+        <h1>
+          Lista de presença
+        </h1>
+        <div className="logo">
+          <strong>João Henrik</strong>  
+          <img src="https://github.com/joaohenrik03.png" alt="Foto do Usuário" />
+        </div> 
+      </header>  
+
+      <label htmlFor="">Digite o nome:</label>
+      <input 
+        type="text" 
+        placeholder='Digite o nome'
+      />
+
+      <button>
+        Adicionar
+      </button>
     </div>
   )
 }
